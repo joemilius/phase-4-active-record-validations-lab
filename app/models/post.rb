a882445e78c9262024afb84a9a13c6bf1a14cd19
JoeMilius <joemilius@gmail.com>
2021-08-10 15:28:29 -0600

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+        validate :title_is_clickbait
+        def title_is_clickbait
+            if ["Won't Believe", "Secret", "Top [number]", "Guess"].none? {|string| title && title.include?(string)}
+                errors.add(:title, "needs more juice")
+            end
+        end
+
 end
